07854302c8ffbe197614713ad2976245fa15fa81
Abdullah Al Bashit <39148573+eeedinar@users.noreply.github.com>
2024-07-05 19:12:41 -0400

    Add files via upload

--- ML/dataloader/lesions.py
+++ ML/dataloader/lesions.py
@@ -7,6 +7,7 @@ import numpy as np
 import glob, sys, os, inspect, ast
 import pandas as pd 
 from sklearn.utils.class_weight import compute_class_weight
+import pylab as plt
 
 # unequal level of list depth/nesting
 def flatten(S):
@@ -70,102 +71,119 @@ def calculate_weights(pos, neg):
 
     return weights
 
+def Iq_scaling(Iq_input, Iq_scale, seek_mf, method = 'MSE'):
+    ll, ul, inc = seek_mf
+    mul_factor  = np.round(np.arange(ll, ul, inc),6)                                  # scaling factor lookup region
+    err = {}; 
+    err['MSE'] = {}
+    err['NEG'] = {}
+    for mf in mul_factor:
+        if method == 'NEG':
+            err['NEG'][mf] = Iq_input - mf*Iq_scale
+        elif method == 'MSE':
+            err['MSE'][mf] = np.mean(np.square(Iq_input - mf*Iq_scale ), axis=1);   # mean square error for scaling
+    mf = np.zeros((Iq_input.shape[0],1))
+    for idx in range(len(mf)):
+        mf[idx] = sorted(err['MSE'].items(), key= lambda x: x[1][idx])[0][0]
+    return mf
+
 class XrayData(Dataset):
 
-    def __init__(self, df, column_names, BNL_dir, sub_dir, lidx=0, uidx=690):
-        ### resetting frames
-        Iq_values     = []
-        labels_out    = []
-        frames_out    = []
-        files_out     = []
+    def __init__(self, df, column_names, BNL_dir, sub_dir, lidx=0, uidx=690, mica_sub=True, mica_Iq = None, tissue_sub=True, tissue_Iq = None, tissue_sub_indices = (360, 420), seek_mf = (-12,12,0.01), scaling=False):
+
+        x_raw      = np.array([])
+        labels_out = np.array([])
+        frames_out = np.array([])
+        files_out  = np.array([]) 
 
         for column_name,label in column_names.items():
+            Iq_values, labels, frames, files =  self.get_intensities(df, column_name, label, BNL_dir, sub_dir)
+            print(f'{column_name} : contains {frames.size} samples')
+            x_raw         = np.vstack([x_raw, Iq_values]) if x_raw.size else Iq_values
+            labels_out    = np.hstack([labels_out, labels]) if labels_out.size else labels
+            frames_out    = np.hstack([frames_out, frames]) if frames_out.size else frames
+            files_out     = np.hstack([files_out, files]) if files_out.size else files
 
-            ### indices where column values are located
-            indices = df[column_name].dropna().index
-            
-            if column_name != "bkg":
-                ### looping over columns
-                for idx in indices:
-                    # print(df['File_Loc'][idx], '--> ', flatten(pd.eval(df[column_name].dropna()[idx])), '-->', ast.literal_eval(df['bkg'][idx]) if type(df['bkg'][idx]) is str else 'NaN' )
-                        file = df['File_Loc'][idx]
-                        df_temp   = pd.read_csv(file, delimiter=",")
+        if mica_sub:
+            if isinstance(mica_Iq, np.ndarray):
+                self.Iq_bkg = mica_Iq
+            else:
+                Iq_bkg , _, _, _ =  self.get_intensities(df, column_name='bkg', label=0, BNL_dir=BNL_dir, sub_dir=sub_dir)
+                self.Iq_bkg = np.mean(Iq_bkg, axis=0)
+                file = open("mica_bkg", "wb")              # Open a binary file in write mode
+                np.save(file, self.Iq_bkg)                 # Save array to the file
+                file.close()                               # Close the file
+            x_raw -= self.Iq_bkg
 
-                        df_frames = flatten( list(ast.literal_eval(df[column_name][idx])) )
+            if tissue_sub:
+                if isinstance(tissue_Iq, np.ndarray):
+                    self.Iq_tissue = tissue_Iq
+                else:
+                    Iq_tissue , _, _, _ =  self.get_intensities(df, column_name='Tissue', label=1, BNL_dir=BNL_dir, sub_dir=sub_dir)
+                    self.Iq_tissue = np.mean(Iq_tissue, axis=0, keepdims=True)
+                    file = open("tissue_Iq", "wb")              # Open a binary file in write mode
+                    np.save(file, self.Iq_tissue)               # Save array to the file
+                    file.close()                                # Close the file
+                
+                tissue_lidx, tissue_uidx = tissue_sub_indices
+                Iq_input = x_raw          [:,tissue_lidx : tissue_uidx]
+                Iq_scale = self.Iq_tissue [:,tissue_lidx : tissue_uidx]
+                self.mf = Iq_scaling(Iq_input, Iq_scale, seek_mf, method = 'MSE')
+                x_raw -= self.mf*self.Iq_tissue
 
-                        Iq_values.append( df_temp.iloc[df_frames].values.tolist() )
-                        labels_out += [label]*len(df_frames)
-                        frames_out += df_frames
-                        files_out  += [file]*len(df_frames)
-
-            elif column_name == "bkg":
-
-                ### Variables for bkg column
-                file_loc = []
-                frames_bkg = []
-                Iq_values_bkg = []
-                for idx in indices:
-
-                    if type(ast.literal_eval( df.iloc[idx][column_name]) ) == dict:
-                        # if not any([file in file_ for file_ in file_loc]):
-                        file   = list(ast.literal_eval( df.iloc[idx][column_name] ).keys())[0]
-                        if not any([file in file_ for file_ in file_loc]):   ## if already added to the stack do not add it again
-                            frames = flatten( list ( ast.literal_eval( df.iloc[idx][column_name]).values()) )
-                    
-                            for file_search in glob.iglob(f'{BNL_dir}/**/*', recursive=True):
-                                if file_search.find(file) > -1 and f'/{sub_dir}/' in file_search:
-                                    file_loc.append(file_search)
-                                    break
-                            frames_bkg.append(frames)
-
-                    else:
-                        file = df.iloc[idx]['File']
-                        if not any([file in file_ for file_ in file_loc]):
-                            frames = ast.literal_eval( df.iloc[idx][column_name])
-                            frames_bkg.append(frames)
-                            file_loc.append(df.iloc[idx]['File_Loc'])
-
-                for file, frame in zip(file_loc, frames_bkg):
-                    df_temp   = pd.read_csv(file, delimiter=",")
-                    
-                    Iq_values_bkg.append( df_temp.iloc[frame].values.tolist() )
-                    labels_out += [label]*len(frame)
-                    frames_out += frame
-                    files_out  += [file]*len(frame)
-
-                temp = []
-                for _ in Iq_values:
-                    temp += _
-                for _ in Iq_values_bkg:
-                    temp += _
-                Iq_values = temp
-
-            try:
-                length = len(np.where(np.array(flatten(labels_out))==label)[0])
-            except:
-                length = len( np.where(np.array(labels_out)==label)[0] )
-            print(f'{column_name}:{label} contains {length} samples')
-
-
-        x_raw = np.vstack(Iq_values)
         x_raw = x_raw[:,lidx:uidx].astype(np.float32)
-        assert np.any(np.isnan(x_raw))==False, "X contains NaN values"
         _, x_raw   = interpolate_missing(x_raw)
-        self.x = x_raw/np.max(x_raw,axis=1).reshape(-1,1)        
+        assert np.any(np.isnan(x_raw))==False, "X contains NaN values"
+        self.x     = x_raw/np.max(x_raw,axis=1).reshape(-1,1) if scaling else x_raw
 
-        self.y         = np.array(labels_out , dtype=np.float32).reshape(-1,1)
+        self.y         = labels_out.astype(np.float32).reshape(-1,1)
         self.n_samples = len(self.y)
         self.frames    = frames_out
         self.files     = files_out
 
+    @staticmethod
+    def get_intensities(df, column_name, label, BNL_dir, sub_dir):
+        ### Variables for bkg column
+        file_loc   = []
+        frames_bkg = []
+        Iq_values  = []
+        labels_out = []
+        frames_out = []
+        files_out  = []
+        indices    = df[column_name].dropna().index
+
+        for idx in indices:
+
+            if type(ast.literal_eval( df.iloc[idx][column_name]) ) == dict:
+                # if not any([file in file_ for file_ in file_loc]):
+                file   = list(ast.literal_eval( df.iloc[idx][column_name] ).keys())[0]
+                if not any([file in file_ for file_ in file_loc]):   ## if already added to the stack do not add it again
+                    frames = flatten( list ( ast.literal_eval( df.iloc[idx][column_name]).values()) )
+
+                    for file_search in glob.iglob(f'{BNL_dir}/**/*', recursive=True):
+                        if file_search.find(file) > -1 and f'/{sub_dir}/' in file_search:
+                            file_loc.append(file_search)
+                            break
+                    frames_bkg.append(frames)
+            else:
+                file = df.iloc[idx]['File']
+                if not any([file in file_ for file_ in file_loc]):
+                    frames = flatten(list(ast.literal_eval( df.iloc[idx][column_name])))
+                    frames_bkg.append(frames)
+                    file_loc.append(df.iloc[idx]['File_Loc'])
+        for file, frame in zip(file_loc, frames_bkg):
+            df_temp     = pd.read_csv(file, delimiter=",", header=0)
+            Iq_values.append( df_temp.iloc[frame].values.tolist() )
+            labels_out += [label]*len(frame)
+            frames_out += frame
+            files_out  += [file]*len(frame)
+        return np.vstack(Iq_values), np.array(labels_out), np.array(frames_out), np.array(files_out)                    
+
     def __getitem__(self, index):
-        # dataset[0]
         return (self.x[index], self.y[index])
 
     def __len__(self):
-        # len(dataset)
-        return self.n_samples
-
+        return self.n_samples   # len(dataset)
 
 
 class triplet_data_generator(XrayData):
@@ -202,8 +220,6 @@ class triplet_data_generator(XrayData):
             self.anc_pos_neg.append( [anc_idx, pos_idx, neg_idx ])
             self.X_APN = np.vstack( (self.X_APN, np.hstack((self.x[anc_idx], self.x[pos_idx], self.x[neg_idx]))) )
         self.x = self.X_APN[1:].astype(np.float32)
-
-
 
 
 def get_dataloaders_random_split(Excel_File, sheet, BNL_dir, sub_dir, column_names, lidx=0, uidx=690):
@@ -308,16 +324,16 @@ def get_sonar_dataloaders(sonar_file):
 
     return weights, training_loader, validation_loader    
 
-def get_dataloaders_fixed_val_files(Excel_File, sheet, BNL_dir, sub_dir, column_names, val_files, lidx=0, uidx=690):
+def get_dataloaders_fixed_val_files(Excel_File, sheet, BNL_dir, sub_dir, column_names, val_files, lidx=0, uidx=690, mica_sub=True, scaling=False):
     batch_size  = 4096
 
     split_dataset    = train_val_split_dataset(get_dataframe_with_files_loc)
     df_train, df_val = split_dataset(Excel_File, sheet, BNL_dir, sub_dir, val_files)
 
     print('Setting Training Dataset ...')
-    dataset_train = XrayData(df_train, column_names, BNL_dir, sub_dir, lidx=lidx, uidx=uidx)
+    dataset_train = XrayData(df_train, column_names, BNL_dir, sub_dir, lidx=lidx, uidx=uidx, mica_sub=mica_sub, scaling=scaling)
     print('Setting Validation Dataset ...')
-    dataset_val   = XrayData(df_val, column_names, BNL_dir, sub_dir, lidx=lidx, uidx=uidx)
+    dataset_val   = XrayData(df_val, column_names, BNL_dir, sub_dir, lidx=lidx, uidx=uidx, mica_sub=mica_sub, scaling=scaling)
 
     # Calculate weights
     weights = compute_class_weight(class_weight="balanced", classes=np.unique(dataset_train.y), y=dataset_train.y.ravel())
@@ -333,15 +349,16 @@ def get_dataloaders_fixed_val_files(Excel_File, sheet, BNL_dir, sub_dir, column_
 if __name__ == '__main__':
     ### specs : names - file , columns
 
-    lidx = 190
-    uidx = 352
-    column_names= {"Tissue":4., "bkg":5.0 }    # {"Diffuse_Plaque":0., "Neurofibrillary_Tangle_(tau)":1. , "Tau":2. ,"Neuritic_Plaque":3., "Tissue":4., "bkg":5.0 }
-    Excel_File  = "/Users/bashit.a/Documents/Alzheimer/Mar-2023/Mar-2023-Samples.xlsx"   # "/home/bashit.a/Codes/ML/Mar-2023-Samples.xlsx"   "/Users/bashit.a/Documents/Alzheimer/Mar-2023/Mar-2023-Samples.xlsx"    sheet       = 'Mar-2023-Samples'
+    lidx = 250
+    uidx = 340
+    column_names= {"Diffuse_Plaque":1. }    # {"Diffuse_Plaque":0., "Neurofibrillary_Tangle_(tau)":1. , "Tau":2. ,"Neuritic_Plaque":3., "Tissue":4., "bkg":5.0 }
+    Excel_File  = "/Users/bashit.a/Documents/Alzheimer/Mar-2023/Mar-2023-Samples-updated.xlsx"   # "/home/bashit.a/Codes/ML/Mar-2023-Samples.xlsx"   "/Users/bashit.a/Documents/Alzheimer/Mar-2023/Mar-2023-Samples.xlsx"    sheet       = 'Mar-2023-Samples'
     BNL_dir     = '/Volumes/HDD/BNL-Data/Mar-2023'    # '/Volumes/HDD/BNL-Data/Mar-2023'         '/scratch/bashit.a/BNL-Data/Mar-2023'
     sub_dir     = "CSV_Conv-8-point"  # CSV_Conv-8-point  CSV
     val_files   = ["1948 V1-roi0_0_0_masked.h5"] # None ["1948_HIPPO-roi1_0_0_masked_intp.h5", "2428-roi1_0_0_masked_intp.h5"]
     sheet       = 'Mar-2023-Samples'
-
+    mica_sub = True
+    scaling  = False
     # print('Random Split Dataset -->')
     # weights, training_loader, validation_loader = get_dataloaders_random_split(Excel_File, sheet, BNL_dir, sub_dir, column_names, lidx=lidx, uidx=uidx)
     # train_dataiter = iter(training_loader)
@@ -353,10 +370,13 @@ if __name__ == '__main__':
     # print(type(val_data), val_data.shape, len(validation_loader))
 
     print('Fixed Validation Dataset -->')
-    weights, training_loader, validation_loader = get_dataloaders_fixed_val_files(Excel_File, sheet, BNL_dir, sub_dir, column_names, val_files, lidx=lidx, uidx=uidx)
+    weights, training_loader, validation_loader = get_dataloaders_fixed_val_files(Excel_File, sheet, BNL_dir, sub_dir, column_names, val_files, lidx=lidx, uidx=uidx, mica_sub=mica_sub, scaling=scaling)
     train_dataiter = iter(training_loader)
     train_data, train_label = next(train_dataiter)
     print(type(train_data), train_data.dtype, train_data.shape, train_label.shape, train_label.dtype, len(training_loader))
+    plt.figure()
+    plt.plot(train_data.detach().numpy().T)
+    plt.show()
 
     val_dataiter = iter(validation_loader)
     val_data, val_label = next(val_dataiter)
